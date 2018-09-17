e17dddc771cfd56589ead9c7e9746f120a720abf
Frederic Theunissen <theunissen@berkeley.edu>
2018-09-17 15:10:32 +0200

    Aggregate Data Frame Done

--- acousticEncoderAgg.py
+++ acousticEncoderAgg.py
@@ -15,12 +15,10 @@ from acousticEncoder import plot_encoder
 batchflg = False  # To run on cluster, otherwise on Frederic's laptop
 
 if batchflg:
-    decomps = ['full_psds', 'spike_rate']
     exp_names = ['YelBlu6903F', 'GreBlu9508M', 'WhiWhi4522M' ]
     agg_file = '/auto/tdrive/mschachter/data/aggregate/encoder_all.pkl'
     plot_me = False
 else:
-    decomps = ['full_psds']   
     exp_names = ['GreBlu9508M']
     agg_file = '/Users/frederictheunissen/Documents/Data/mschachter/aggregate/encoder_all.pkl'
     plot_me = True
@@ -34,51 +32,51 @@ for exp_name in exp_names:
     else:            
         preproc_dir = '/Users/frederictheunissen/Documents/Data/mschachter/%s/preprocess' % exp_name
         encoder_dir = '/Users/frederictheunissen/Documents/Data/mschachter/%s/encoder' % exp_name
-    
-    # Make the output directory if it does not exist.
-    if not os.path.exists(encoder_dir):
-        os.makedirs(encoder_dir)
-        
+      
+    # Find all the output files in the decoder folders.    
     seg_list = []
-    for fname in os.listdir(preproc_dir):
-        for decomp in decomps:
-            if fname.endswith('R_%s.h5' % decomp) and fname.startswith('preproc_'):
-                segname = fname.split('_')[1] + '_' +fname.split('_')[2] + '_' + fname.split('_')[3]
-                seg_list.append(segname)
-            if fname.endswith('L_%s.h5' % decomp) and fname.startswith('preproc_'):
-                segname = fname.split('_')[1] + '_' +fname.split('_')[2] + '_' + fname.split('_')[3]
-                seg_list.append(segname)
+    dec_list = []
+    fil_list = []
+    for fname in os.listdir(encoder_dir):
+        if fname.endswith('.pkl'):
+            segname = fname.split('_')[1] + '_' + fname.split('_')[2] + '_' + fname.split('_')[3]
+            decomp = fname.split('_')[4] + '_' + fname.split('_')[5]
+            decomp = decomp.split('.')[0]
+            fil_list.append(fname)
+            seg_list.append(segname)
+            dec_list.append(decomp)
                 
+    for i, fname in enumerate(fil_list):
+        
+        # Each output file corresponds to one experiment, one site and one neural decomposition
+        segname = seg_list[i]
+        decomp  = dec_list[i]
+        
+        # Input and Output files of the decoder
+        preproc_file = os.path.join(preproc_dir, 'preproc_%s_%s.h5' % (segname, decomp))
+        encoder_file = os.path.join(encoder_dir, '%s' % fname )
 
-    for seg_uname in seg_list:
-        for decomp in decomps:
-            
-            # Input and Output files of the decoder
-            preproc_file = os.path.join(preproc_dir, 'preproc_%s_%s.h5' % (seg_uname, decomp))
-            output_file = os.path.join(encoder_dir, 'encoder_%s_%s.pkl' % (seg_uname, decomp))
-
-           # Read the results         
-            resultsDataFrame = pd.read_pickle(output_file)
+        # Read the results         
+        resultsDataFrame = pd.read_pickle(encoder_file)
            
-           # Add columns to specify origin of theresults  
-            nrows = len(resultsDataFrame.index)
-            resultsDataFrame['experiment'] = exp_name
-            resultsDataFrame['site'] = seg_uname
-            resultsDataFrame['decomp'] = decomp
+        # Add columns to specify origin of theresults  
+        nrows = len(resultsDataFrame.index)
+        resultsDataFrame['experiment'] = exp_name
+        resultsDataFrame['site'] = segname
+        resultsDataFrame['decomp'] = decomp
             
            
-            # Plotting all the results
-            if plot_me:
-                plot_encoder(preproc_file, resultsDataFrame)
+        # Plotting all the results
+        if plot_me:
+            plot_encoder(preproc_file, resultsDataFrame)
 
-            if iframe == 0:
-                resultsAgg = resultsDataFrame
-            else:
-                resultsAgg = pd.concat([resultsAgg, resultsDataFrame], ignore_index = True )
+        if i == 0:
+            resultsAgg = resultsDataFrame
+        else:
+            resultsAgg = pd.concat([resultsAgg, resultsDataFrame], ignore_index = True )
             
-            iframe += 1
 
-print('%d dataframes were aggregated' % (iframe +1))
+print('%d dataframes were aggregated' % (i +1))
 
 # Saving aggregate data file
 resultsAgg.to_pickle(agg_file)
